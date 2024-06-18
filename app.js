f3c165f0fca7cd90c2b4229bdc0fc5b01858a50d
CyberMan010 <mohamahasoun60@gmail.com>
2024-06-18 20:02:54 +0300

    asdawdv

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 // map method #1
-function mid(){
-    let input = prompt("Type a nummer");
-    let arr = input.split(" ");
-    let square = input => Math.pow(input,2);
-    square = arr.map(square);
+// let input = prompt("Type a nummer");
+// let arr = input.split(" ");
+// function mid(arr) {
+//     let square = input => Math.pow(2,input);
+//     square = arr.map(square);
     
-    
-    console.log(arr);
-    console.log(square);
-}
-mid();
+//     console.log(arr);
+//     console.log(square);
+// }
+// mid(arr);
 // --------------forEach method-------------------
+// let input = prompt("Type a nummer");
+// let arr = input.split(" ");
+// let array = [];
+// function mid(arr) {
+//     let square = input => array.push(Math.pow(2,input));
+//     arr.forEach(square);
+//     console.log(arr);
+//     console.log(array);
+// }
+// mid(arr);
+// --------for--------
+// let input = prompt("Type a nummer");
+// let arr = input.split(" ");
+// let array = [];
+// function mid(arr) {
+    
+//     for (let i = 0; i < arr.length; i++){
+//         array.push(Math.pow(2, arr[i]));
+//     }
+//     console.log(arr);
+//     console.log(array);
+// }
+// mid(arr);
+// ---------------task2-------------
+let input = prompt("Type a nummer");
+let arr = input.split(" ");
+function fun() {
+    
+}
