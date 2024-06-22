377565162a4dd0c543f91181169d34a6ae80ff99
CyberMan010 <mohamahasoun60@gmail.com>
2024-06-22 22:34:24 +0300

    happy nation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Higher-Order-Function-
+*This is one of the most joyable manipulation exercises you could have done*  
+# Task-1:
+_you should make an array and exchange any number you put in and multiplie it by 2 , therefore you need to finish it with 3 ways (map,foreach and for methods)_
+
+# Task-2:
+_Use loop and map to bring back a new array with decleration of each input whetherwise it's integer or string(integers should be defined by even or odd)_
+
+# Task-3:
+_I choosed forEach because I tried map() in Task one , this task was simple and I learnt how to display name through the array_
+
+# Task-4:
+_This one seems like a game , I tried to make a for loop first ,therefore it didn't work for me ,so I tried another way with if statement and forEach so if the number in the input can be divisible by 3 it says Fizz if it was with 5 it says Buzz , if both it says FizzBuzz and give us an output_
+
+*Thanks for reading :)* 
+
--- app.js
+++ app.js
@@ -34,8 +34,51 @@
 // }
 // mid(arr);
 // ---------------task2-------------
-let input = prompt("Type a nummer");
-let arr = input.split(" ");
-function fun() {
+
+// let even = [5,2,"name"];
+// function mid(arr) {
+//     return arr.map(function (num) {
+//         if (typeof num !== "number") {
+//             return 'N/A';
+//         } else if (num % 2 == 0) {
+//             return 'even';
+//         } else {
+//             return 'odd';
+//         }
+//     })
     
-}
+    
+   
+// }
+// let result = mid(even);
+// console.log(result);
+
+// -------task3-----------
+// const arr = ["Rawan", "Wesam", "Hind", "Muhammed", "Esraa", "Dareen"]
+// arr.forEach((arr) => {
+//     console.log(arr);
+// });
+// --------Task4----------
+
+// function fizzbuzz(array) {
+//     let output = [];
+   
+//     array.forEach((fixing) => {
+//         let number = parseInt(fixing, 10);
+//         if ((number) % 5 == 0 && number % 3 == 0) {
+//             output.push("Fizz Buzz");
+//         } else if (number % 5 == 0) {
+//             output.push("Buzz");
+//         } else if (number % 3 == 0) {
+//             output.push("Fizz");
+//         } else {
+//             output.push(number);
+//         }
+//     });
+//     return output;
+       
+
+   
+// }
+
+// console.log(fizzbuzz(prompt().split(" ")));
